3ae7bd12c6d2b7535a9d7b443cd39ea79111b8b6
Nzamiey@98 <Nondzamekonosisi@gmail.com>
2023-06-27 14:40:34 -0700

    updated@

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -17,20 +17,20 @@ void sort_deck(deck_node_t **deck);
  */
 int _strcmp(const char *s1, const char *s2)
 {
-        /* Compare characters until a difference is found or the end of a string is reached */
-        while (*s1 && *s2 && *s1 == *s2)
-        {
-                 /* Move to the next character in s1 */
-                s1++;
-                /* Move to the next character in s2 */
-                s2++;
-        }
+	/* Compare characters until a difference is found or the end of a string is reached */
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		 /* Move to the next character in s1 */
+		s1++;
+		/* Move to the next character in s2 */
+		s2++;
+	}
 
-        if (*s1 != *s2)
-                /* Return the difference between the corresponding characters in s1 and s2 */
-                return (*s1 - *s2);
-         /* If s1 and s2 are equal, return 0 */
-        return (0);
+	if (*s1 != *s2)
+		 /* Return the difference between the corresponding characters in s1 and s2 */
+		 return (*s1 - *s2);
+	 /* If s1 and s2 are equal, return 0 */
+	return (0);
 }
 
 /**
@@ -41,7 +41,7 @@ int _strcmp(const char *s1, const char *s2)
  */
 char get_value(deck_node_t *card)
 {
-        if (_strcmp(card->card->value, "Ace") == 0)
+	if (_strcmp(card->card->value, "Ace") == 0)
                 return (0);  /* If the card value is "Ace", return 0 */
 
         if (_strcmp(card->card->value, "1") == 0)
